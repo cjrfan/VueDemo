68309798dae1fa5657fb5fcda24d55b646218be7
cjrfan <2298579045@qq.com>
2017-07-02 18:42:36 +0800

    v-router 1px边框

--- sale/src/App.vue
+++ sale/src/App.vue
@@ -1,19 +1,25 @@
 <template>
-  <div id="app">
+  <div>
     <v-header></v-header>
-    <div class="tab">
-      <div class="tab-item">商品</div>
-      <div class="tab-item">评论</div>
-      <div class="tab-item">商家</div>
+    <div class="tab border-1px">
+      <div class="tab-item">
+        <!--使用指令v-link进行导航-->
+        <a v-link="{path:'/goods'}">商品</a>
+      </div>
+      <div class="tab-item">
+        <a v-link="{path:'/ratings'}">评论</a>
+      </div>
+      <div class="tab-item">
+        <a v-link="{path:'/seller'}">商家</a>
+      </div>
     </div>
-    <div class="content">
-      I am content!
-    </div>
+    <!--路由外链，路由匹配到的组件将渲染在这里-->
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-  import header from './components/header/header.vue';
+  import header from 'components/header/header';
 
   export default{
     components: {
@@ -22,17 +28,24 @@
   };
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
-  #app
-    .tab
-      /*移动端常用等分布局*/
-      display: flex
-      width: 100%
-      /*移动端大小是网页端的2倍*/
-      height: 40px
-      line-height: 40px;
-      .tab-item
-        flex: 1
-        text-align: center
+  @import "common/stylus/mixin.styl"
 
+  .tab
+    /*移动端常用等分布局*/
+    display: flex
+    width: 100%
+    /*移动端大小是网页端的2倍*/
+    height: 40px
+    line-height: 40px;
+    border-1px(rgba(7, 17, 27, 0.1))
+    .tab-item
+      flex: 1
+      text-align: center
+      & > a
+        display: block
+        cursor: pointer
+        color: rgb(77, 85, 93)
+        &.active
+          color: rgb(240, 20, 20)
 
 </style>
--- sale/src/main.js
+++ sale/src/main.js
@@ -1,8 +1,39 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App';
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: {App}
+import goods from 'components/goods/goods';// 等价于components/goods/goods.vue
+import ratings from 'components/ratings/ratings';
+import seller from 'components/seller/seller';
+
+import 'common/stylus/index.styl';
+
+// 通过 Vue.use() 安装路由模块
+Vue.use(VueRouter);
+
+// Vue.extend(App) 代替new Vue() , 这里Vue.extend 和new Vue 搞不清
+let app = Vue.extend(App);
+
+// 创建一个路由器实例
+let router = new VueRouter({
+  linkActiveClass: 'active' // 设置激活状态的class
 });
+
+// 定义路由规则
+router.map({
+  '/goods': {
+    component: goods
+  },
+  '/ratings': {
+    component: ratings
+  },
+  '/seller': {
+    component: seller
+  }
+});
+
+// 路由器会创建一个 app 实例，并且挂载到选择符 #app 匹配的元素上。
+router.start(app, '#app');
+
+// 默认打开页面时 路由跳转
+router.go('/goods');
